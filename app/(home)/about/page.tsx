910b8373ece7a9b5ca2e4b5d84cbf2e3104aad8e
hsiaoju <kelllllyko@gmail.com>
2025-05-06 00:10:29 +0800

    feat(about): style game UI

--- app/(home)/about/page.tsx
+++ app/(home)/about/page.tsx
@@ -1,51 +1,72 @@
 'use client'
 
 import { useState, useRef } from 'react'
-import { getStartDay, generateAnswer } from '../../utils/'
-import CodeBox from '../../ui/CodeBox'
+import { getStartDay, generateAnswer, getHint } from '../../utils/'
+import CodeBox from '../../components/CodeBox'
 import cx from 'clsx'
 import Button from '../../ui/Button'
 import Input from '../../ui/Input'
+import GuessList from '../../components/GuessList'
+
+type guessType = {
+    guess: number
+    hint: string
+}
+
+const widthClasses = 'mx-auto w-11/12 max-w-lg md:w-full'
 
 export default function About() {
     const [isStart, setIsStart] = useState<boolean>(false)
-    const [guessList, setGuessList] = useState<number[]>([])
+    const [guessList, setGuessList] = useState<guessType[]>([])
     const [answer, setAnswer] = useState<number[]>([])
-    console.log('🚀 - About - answer:', answer)
+    const [isCorrect, setIsCorrect] = useState<boolean>(false)
     const inputRef = useRef<HTMLInputElement>(null)
-    let A = 0
-    let B = 0
 
     const start = getStartDay(new Date('2022/02/04'))
     const now = getStartDay(new Date())
     const msPerDay = 1000 * 60 * 60 * 24
     const days = Math.floor((now - start) / msPerDay)
 
-    const width = 'mx-auto w-11/12 max-w-lg md:w-full'
-
     const gameStart = () => {
-        setAnswer(generateAnswer())
+        const generated = generateAnswer()
+        setAnswer(generated)
         setIsStart(true)
+        setGuessList([])
+        setIsCorrect(false)
+
+        console.log(
+            "%c sssh, it's secret: %c" + generated.join(''),
+            'color: #46c8dc; font-weight: bold; font-size: 12px;',
+            'color: #fff; font-weight: bold; font-size: 16px;',
+        )
     }
 
     const handleSubmit = () => {
-        console.log('inputRef', typeof inputRef.current?.value) // string
-        const value = inputRef.current?.value.split('')
+        const input = inputRef.current
+        if (!input) return
+
+        const value = input.value.split('')
         if (!value) return
 
-        value.forEach((num, i) => {
-            if (Number(num) === answer[i]) {
-                A++
-            } else if (answer.includes(Number(num))) {
-                B++
-            }
-        })
+        const hint = getHint(value, answer)
+        const guess = Number(value.join(''))
+
+        setGuessList((prev) => [...prev, { guess, hint }])
+
+        input.value = ''
+
+        if (hint === '4A0B') {
+            setIsCorrect(true)
+        }
     }
 
     return (
-        <div className='flex flex-col justify-between gap-6 lg:flex-row'>
+        <div className='mb-8 flex flex-col-reverse justify-between gap-6 lg:flex-row'>
             <div
-                className={cx('relative overflow-hidden rounded-xl text-xl', width)}
+                className={cx(
+                    'relative overflow-hidden rounded-xl text-xl',
+                    widthClasses,
+                )}
             >
                 <CodeBox
                     language='javascript'
@@ -65,8 +86,8 @@ export default function About() {
 <p id="code"> Days: ${days} </p>`}
                 />
             </div>
-            <p className='mt-6 text-center text-4xl'>∞</p>
-            <div className={cx('flex flex-col', width)}>
+            <p className='mt-6 hidden text-center text-4xl md:block'>∞</p>
+            <div className={cx('flex flex-col', widthClasses)}>
                 <div
                     className={cx(
                         'inset-shadow-t-light flex h-12 w-full items-center gap-4 rounded-t-xl px-4',
@@ -90,29 +111,47 @@ export default function About() {
                         <p className='mb-2 text-xl'>
                             Guess the secret 4-digit number.
                         </p>
-                        <p className='bg-canary rounded-full px-5 py-3 text-lg leading-6'>
+                        <p
+                            className={cx(
+                                'rounded-3xl px-5 py-3 text-lg leading-6 md:rounded-full',
+                                isStart ? 'bg-canary' : 'bg-blue',
+                            )}
+                        >
                             You only have 12 chances to guess the secret number. Use
                             your guesses wisely!
                         </p>
                     </div>
                     {isStart ? (
-                        <div className='mx-4 mt-5 flex gap-5 text-xl'>
-                            <Input
-                                ref={inputRef}
-                                className='w-1/2 rounded-full bg-white px-4 text-center tracking-widest placeholder:text-stone-300 focus:placeholder-transparent'
-                                placeholder='ex: 1234'
-                            />
-                            <Button
-                                onClick={handleSubmit}
-                                className='bg-coral hover:text-shadow-hover w-1/2 rounded-full px-6 py-3 hover:shadow-white'
-                            >
-                                Submit
-                            </Button>
-                        </div>
+                        <>
+                            <div className='flex-between mx-4 mt-5 gap-5 text-xl'>
+                                <Input
+                                    ref={inputRef}
+                                    className='w-full rounded-full bg-white px-4 py-3 text-center tracking-widest placeholder:text-stone-300 focus:placeholder-transparent'
+                                    placeholder='ex: 1234'
+                                    maxLength={4}
+                                    type='number'
+                                />
+                                <Button
+                                    onClick={handleSubmit}
+                                    className='bg-coral hover:text-shadow-hover w-1/3 rounded-full px-6 py-3 hover:shadow-white md:w-1/2'
+                                >
+                                    Try!
+                                </Button>
+                            </div>
+                            <GuessList list={guessList} />
+                            {isCorrect && (
+                                <Button
+                                    onClick={gameStart}
+                                    className='bg-blue hover:text-shadow-hover inset-shadow-btn_default mx-auto my-2 rounded-full px-6 py-2 text-xl hover:shadow-white'
+                                >
+                                    Congrats&ensp;🎉🎉🎉
+                                </Button>
+                            )}
+                        </>
                     ) : (
                         <Button
                             onClick={gameStart}
-                            className='bg-blue hover:text-shadow-hover m-auto rounded-full px-6 py-3 text-xl hover:shadow-white'
+                            className='bg-canary hover:text-shadow-hover mx-auto my-4 rounded-full px-6 py-3 text-xl hover:shadow-white lg:my-auto'
                         >
                             Game Start
                         </Button>
---
+++ app/components/GuessList.tsx
@@ -0,0 +1,30 @@
+import cx from 'clsx'
+
+export type guessType = {
+    guess: number
+    hint: string
+}
+
+interface GuessListProps {
+    list: guessType[]
+    className?: string
+}
+
+export default function GuessList({ list, className }: GuessListProps) {
+    return (
+        <div
+            className={cx(
+                className ??
+                    'font-code mx-auto mt-2 grid gap-x-6 p-2 text-xl md:grid-cols-2',
+            )}
+        >
+            {list.map((item, index) => (
+                <div className='flex gap-1 tracking-wider' key={index}>
+                    <span className='text-stone-500'>{index + 1}: </span>
+                    <span className='font-bold'>{item.guess}&ensp;</span>
+                    <span className='font-medium text-black'>{item.hint}</span>
+                </div>
+            ))}
+        </div>
+    )
+}
