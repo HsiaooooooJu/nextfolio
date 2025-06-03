19142b446f3c74343ffc141a6ca7a4feba56e567
HsiaooooooJu <kelllllyko@gmail.com>
2025-06-03 11:47:44 +0800

    fix(home): clear timerRef

--- app/hooks/useDebounce.ts
+++ app/hooks/useDebounce.ts
@@ -18,6 +18,7 @@ export default function useDebounce<T extends unknown[]>(
             }
             timer.current = setTimeout(() => {
                 savedFn.current(...args)
+                timer.current = null
             }, delay)
         },
         [delay],
